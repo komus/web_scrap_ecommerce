1144f0ac46eacf71b97966beec7752590b9a9d87
oyindolapo <oyindolapokomolafe@yahoo.com>
2021-07-02 08:40:41 +0300

    remove file

--- src/scrap_sites.py
+++ src/scrap_sites.py
@@ -2,6 +2,6 @@ from modules.web_scrap import scrap_ecommerce_amazon, write_to_csv
 from dbfuncs import dml_insert_into_data,dml_fetch_products
 
 data = scrap_ecommerce_amazon(3000, 65, 'dress')
-print(data.shape)
+#print(data.shape)
 dml_insert_into_data(data)
 write_to_csv('amazon_products', dml_fetch_products())
